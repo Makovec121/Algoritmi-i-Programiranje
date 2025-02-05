6b195abcade24d6ab4a8f207ef1af33ad538c349
Makovec121 <makovecdominik799@gmail.com>
2025-02-05 11:15:38 +0100

    Add files via upload

--- 05/02/main.cpp
+++ 05/02/main.cpp
@@ -2,26 +2,13 @@
 
 using namespace std;
 
-int main(){
- char odabir;
- int v1, v2;
- scanf("%c %i %i", &odabir, &v1, &v2);
- if (odabir=='R'){
-    int U=v1;
-    int I=v2;
-    int R=U/I;
-        cout<<R;}
-else if (odabir=='U'){
-    int R=v1;
-    int I=v2;
-    int U=R*I;
-        cout<<U;}
-else if (odabir=='I'){
-    int R=v1;
-    int U=v2;
-    int I=U/R;
-        cout<<I;}
-
+int main()
+{
+    float a;
+    cin>>a;
+    float b;
+    b=a*0.25+a;
+        cout<<"Cijena s pdv-om je: "<<b;
 
     return 0;
 }
